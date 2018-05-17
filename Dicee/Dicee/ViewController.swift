5485276a17bd575cf82dca4c352f091e449dc895
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-05-16 22:21:13 -0400

    Added random dice faces every time app is opened. Added a updateDice function.

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    // Declare holders for random numbers
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    // Funciton called immediately when view is loaded.
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Generate random dice faces each time the app is opened.
+        updateDice()
     }
 
     override func didReceiveMemoryWarning()
@@ -29,8 +33,20 @@ class ViewController: UIViewController
     }
 
 
-    // Listner function for button press.
+    /*
+        Listner function for button press.
+        Updates dice image upon button press.
+     */
     @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+        // Upon button press, update dice
+        updateDice()
+    }
+    
+    /*
+        This function updates the images of the dice on the screen.
+    */
+    func updateDice()
     {
         randomDiceIndex1 = Int(arc4random_uniform(6)) + 1
         randomDiceIndex2 = Int(arc4random_uniform(6)) + 1
